9de92d38a33d5f8f10c64c44a17addcb2485cfed
Rafael George <george.rafael@gmail.com>
2025-02-18 10:22:05 -0700

    ref: add test for scrape.Scrape

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"time"
+	"io"
 )
 
 type Scrapper struct {
--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"testing"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 )
 
 func TestNewScrapper(t *testing.T) {
@@ -13,4 +16,26 @@ func TestNewScrapper(t *testing.T) {
 	if scrapper.client == nil {
 		t.Error("Expected scrapper.client to be created, but got nil")
 	}
+}
+
+func TestScrape(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("<html><body>Hello, world!</body></html>"))
+	}))
+
+	defer server.Close()
+
+	scrapper := NewScrapper()
+	content, err := scrapper.Scrape(server.URL)
+
+	if err != nil {
+		t.Errorf("Expected no error, but got %v", err)
+	}
+
+	if !strings.Contains(content, "Hello, world!") {
+		t.Errorf("Expected content to contain 'Hello, world!', but got %s", content)
+	}
+
+
 }
